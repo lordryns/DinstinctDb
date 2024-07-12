45894587da8504bfd0e98abafa43dc5f2e612a0a
David <107300355+lordryns@users.noreply.github.com>
2024-07-12 23:21:53 +0100

    Update main.py
    Removed key parameter for the create_db endpoint, instead now generates a random key for the user.

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
-import json 
+import json, uuid
 
 app = FastAPI()
 
-
-class NewDbModel(BaseModel):
-    key: str
 
 @app.get("/")
 def home():
     return {"title": "DistinctDB"}
 
 @app.post("/create_db")
-def create_db(model: NewDbModel):
-    with open(f"db/db{model.key}.json", 'w') as fp:
+def create_db():
+    key = str(uuid.uuid4())
+    
+    with open(f"db/db{key}.json", 'w') as fp:
         fp.write("{}")
     
     return  {
         "message": f"Database created successfully, keep your Key safe!",
-        "key": model.key
+        "key": key
     }
+
+
+@app.post("/add_keys")
+def add_keys():
+    pass
