d54aa01ad43d0d21a48db05359b0e082a8c7c0e5
David <107300355+lordryns@users.noreply.github.com>
2024-07-12 23:48:32 +0100

    Update main.py
    Now allows users to add to the database.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
 import json, uuid
+from typing import Union
 
 app = FastAPI()
 
+class CreateKeyModel(BaseModel):
+    db_key: str
+    title: str
+    value: str
+    data_type: Union["str", "int", "bool", "float"]
 
 @app.get("/")
 def home():
@@ -18,10 +24,29 @@ def create_db():
     
     return  {
         "message": f"Database created successfully, keep your Key safe!",
-        "key": key
+        "key": key, 
+        "success": True
     }
 
 
-@app.post("/add_keys")
-def add_keys():
-    pass
+@app.post("/add_key")
+def add_keys(key_model: CreateKeyModel):
+    try: 
+        with open(f"db/db{key_model.db_key}.json", 'r') as fp:
+            pass
+
+        with open(f"db/db{key_model.db_key}.json", 'w') as fp:
+            json.dump({key_model.title: key_model.value}, fp)
+
+        return {
+            "message": "Value added successfully!",
+
+        "success": True
+    }
+
+    except Exception as e:
+        return {
+            "message": f"Database not found! Raw exception: {e}",
+
+        "success": False
+        }
