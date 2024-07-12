e18e970ac6fcdc6be761e6a416e28999e91a713d
David <rynstheoverlord@gmail.com>
2024-07-12 05:48:54 +0100

    Finished up the create_db route!

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ def home():
 def create_db(key: str):
     with open(f"db/db{key}.json") as fp:
         fp.write("{}")
+    
+    return  {
+        "message": f"Database created successfully, keep your Key safe!",
+        "key": key
+    }
